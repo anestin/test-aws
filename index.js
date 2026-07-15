ead64953a087e360b85e5641d9d0ce6fe810de01
anestin <108366846+anestin@users.noreply.github.com>
2026-07-15 15:24:52 +0100

    Fix typo in JSON.stringify call

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
-fs.writeFileSync('about.txt',JSON.strigify({"name":"anestin"}))
+fs.writeFileSync('about.txt',JSON.stringify({"name":"anestin"}))
 const content = fs.readFileSync("about.txt")
 console.log(content)
