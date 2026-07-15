0cf3d045ac53f35bba398f64675fac721f5620f1
anestin <108366846+anestin@users.noreply.github.com>
2026-07-15 15:22:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 fs.writeFileSync('about.txt',JSON.strigify({"name":"anestin"}))
-cosnt content = fs.readFileSync("about.txt")
+const content = fs.readFileSync("about.txt")
 console.log(content)
